068c90aed1fce144d316689cc46ee6595963b2d3
JK <juergen.krauss@reutlingen-university.de>
2020-09-21 14:50:43 +0200

    finished Section 5

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  constructor() {
+    console.log('constructor called!')
+  }
+
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!')
+  }
+
+
+}
