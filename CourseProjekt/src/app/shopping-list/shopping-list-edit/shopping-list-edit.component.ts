7dda20678e2e8a7d08f7503dd7d728c7f52f4234
JK <juergen.krauss@reutlingen-university.de>
2020-09-22 21:43:56 +0200

    finished 89

--- CourseProjekt/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ CourseProjekt/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Recipe } from 'src/app/recipes/recipe.model';
+import { Ingredient } from 'src/app/recipes/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: true}) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem(){
+    this.ingredientAdded.emit({
+      name: this.nameInputRef.nativeElement.value,
+      amount: this.amountInputRef.nativeElement.value}
+    )
+  }
 }
